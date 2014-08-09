ad16eea4b6326dbc5184d66ed16d53f5374e4d51
James Beal <james.beal@sanger.ac.uk>
2014-08-09 20:58:06 +0100

    Initial work on automating new vagrant systems

---
+++ scripts/mysql_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+# Install mysql
+echo 'mysql-server mysql-server/root_password password root' |  sudo debconf-set-selections
+echo 'mysql-server mysql-server/root_password_again password root' | sudo debconf-set-selections
+sudo apt-get -y -q install  mysql-server-5.6
---
+++ scripts/vagrant_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Installing vagrant keys
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+cd ~/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+chown -R vagrant ~/.ssh
+
+# RVM Install
+wget --retry-connrefused -q -O - https://get.rvm.io | bash -s stable
+source /home/vagrant/.rvm/scripts/rvm
+
+
