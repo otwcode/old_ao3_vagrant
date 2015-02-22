fa4c616b00683f45595614aa7af5fe3546e0bf93
zz9pzza <zz9pzza@gmail.com>
2015-02-22 23:01:20 +0000

    catch up

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -8,7 +8,26 @@ cleanup() {
 }
 trap "cleanup" INT TERM EXIT
 rm -f /home/vagrant/VBoxGuestAdditions*.iso
-rm -f /var/cache/apt/archives/*.deb
-dd if=/dev/zero of=/EMPTY bs=1M 
+# Remove APT cache
+apt-get clean -y
+apt-get autoclean -y
+
+
+#cleanup log files
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
+
+# Whiteout /boot
+count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
+let count--
+dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
+rm /boot/whitespace;
+
+swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
+swapoff $swappart;
+dd if=/dev/zero of=$swappart;
+mkswap $swappart;
+swapon $swappart;
+
+
 rm -f /EMPTY
 exit 0
--- scripts/vagrant_setup.sh
+++ scripts/vagrant_setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+mkdir /tmp/vbox
+VER=$(cat /home/vagrant/.vbox_version)
+wget -nc http://download.virtualbox.org/virtualbox/$VER/VBoxGuestAdditions_$VER.iso
+mount -o loop VBoxGuestAdditions_$VER.iso /tmp/vbox
+sh /tmp/vbox/VBoxLinuxAdditions.run
+umount /tmp/vbox
+rmdir /tmp/vbox
+rm *.iso
+
 # Installing vagrant keys
 mkdir ~/.ssh
 chmod 700 ~/.ssh
